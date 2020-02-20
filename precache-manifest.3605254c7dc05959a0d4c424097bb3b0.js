d4943665518b234b13b5229d852e30c05008b207
MiVladie <mivladie@github.com>
2020-02-20 09:31:41 +0000

    Updates

---
+++ precache-manifest.3605254c7dc05959a0d4c424097bb3b0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5b6c515c231213765bfff564e2f949",
+    "url": "/beautyatluxx/index.html"
+  },
+  {
+    "revision": "236739a753de35307e39",
+    "url": "/beautyatluxx/static/css/main.f4acacde.chunk.css"
+  },
+  {
+    "revision": "51dd34869417be35e27e",
+    "url": "/beautyatluxx/static/js/2.8738aa1d.chunk.js"
+  },
+  {
+    "revision": "e338a9c1a776fa39a27c68d543c035fb",
+    "url": "/beautyatluxx/static/js/2.8738aa1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236739a753de35307e39",
+    "url": "/beautyatluxx/static/js/main.c47911ef.chunk.js"
+  },
+  {
+    "revision": "1d61088f60b202d06df7",
+    "url": "/beautyatluxx/static/js/runtime-main.364e8e96.js"
+  },
+  {
+    "revision": "a50d9ec08116aad90999cb8a948a6c94",
+    "url": "/beautyatluxx/static/media/banner_01.a50d9ec0.jpeg"
+  },
+  {
+    "revision": "f80c4d1383d4a36f1421b345e44045b2",
+    "url": "/beautyatluxx/static/media/banner_02.f80c4d13.jpeg"
+  },
+  {
+    "revision": "968a831fb1f221520ac2f7ddc6caef40",
+    "url": "/beautyatluxx/static/media/banner_03.968a831f.jpeg"
+  },
+  {
+    "revision": "efc57384d7fb36818de3551c39022a7e",
+    "url": "/beautyatluxx/static/media/body.efc57384.png"
+  },
+  {
+    "revision": "a54142f41248124a1d18d6c2b2d6380a",
+    "url": "/beautyatluxx/static/media/image2.a54142f4.jpeg"
+  },
+  {
+    "revision": "a7e9d560e92c8bac2c442833cbdb1381",
+    "url": "/beautyatluxx/static/media/image3.a7e9d560.jpeg"
+  },
+  {
+    "revision": "7908d9e78ec4256ecc137607603d2e4d",
+    "url": "/beautyatluxx/static/media/image4.7908d9e7.jpeg"
+  },
+  {
+    "revision": "ab518e632f8741b9ea21024fed85f467",
+    "url": "/beautyatluxx/static/media/image5.ab518e63.jpeg"
+  },
+  {
+    "revision": "93d2b1e4c5523ec46ee3f9e5f1db7d1c",
+    "url": "/beautyatluxx/static/media/image6.93d2b1e4.jpeg"
+  },
+  {
+    "revision": "a5e2c948ff118cbddf9b82a85854422a",
+    "url": "/beautyatluxx/static/media/logo.a5e2c948.png"
+  },
+  {
+    "revision": "6d1d525e31d71cfba27d117e3d1605c1",
+    "url": "/beautyatluxx/static/media/makeup.6d1d525e.png"
+  },
+  {
+    "revision": "5a4812b87152beeec83b40b64f9898f8",
+    "url": "/beautyatluxx/static/media/massage.5a4812b8.png"
+  },
+  {
+    "revision": "7cd24564c618f002cbad677fae8efefe",
+    "url": "/beautyatluxx/static/media/nails.7cd24564.png"
+  },
+  {
+    "revision": "1f9961ea42cbb6545e90deeb0dd194f4",
+    "url": "/beautyatluxx/static/media/salon.1f9961ea.jpeg"
+  }
+]);
