4eca17ac5d2b40661db0e997cb427aa40656af80
MiVladie <justvladon@outlook.com>
2022-12-11 14:27:33 +0200

    minor updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,32 +6,44 @@ import logo from '../../assets/icons/logo.png';
 import classes from './Footer.module.scss';
 
 const Footer = () => {
-    const [admin, setAdmin] = useState(false);
-    
-    const switchAdmin = () => {
-        document.body.style.overflow = !admin ? 'hidden' : 'visible';
-        setAdmin(!admin);
-    }
+	const [admin, setAdmin] = useState(false);
 
-    const closePanel = () => {
-        document.body.style.overflow = 'auto';
-        setAdmin(false);
-    }
+	const switchAdmin = () => {
+		document.body.style.overflow = !admin ? 'hidden' : 'visible';
+		setAdmin(!admin);
+	};
 
-    return (
-    	<div className = { classes.Footer }>
-            <div className = { classes.Wrapper }>
-                <img className = { classes.Logo } src = { logo } alt = 'Beauty Salon Ealing Logo' />
-                <div className = { classes.Content }>
-                    <p>Copyright Beauty at Luxx &copy; { new Date().getFullYear() } <br className = { classes.Break } />All Rights Reserved</p>
-                    <p>Website created by <span><a href = 'https://letscomit.com/' target = '_blank' rel = 'noopener noreferrer'>letscomit</a></span></p>
-                </div>
-                <button className = { classes.Admin } onClick = { switchAdmin }>Admin</button>
-            </div>
+	const closePanel = () => {
+		document.body.style.overflow = 'auto';
+		setAdmin(false);
+	};
 
-            { admin ? <Admin closePanel = { () => closePanel() } /> : null }
-    	</div>
-    );
-}
+	return (
+		<div className={classes.Footer}>
+			<div className={classes.Wrapper}>
+				<img className={classes.Logo} src={logo} alt='Beauty Salon Ealing Logo' />
+				<div className={classes.Content}>
+					<p>
+						Copyright Beauty at Luxx &copy; {new Date().getFullYear()} <br className={classes.Break} />
+						All Rights Reserved
+					</p>
+					<p>
+						Website created by{' '}
+						<span>
+							<a href='https://mivladie.github.io/letscomit/' target='_blank' rel='noopener noreferrer'>
+								letscomit
+							</a>
+						</span>
+					</p>
+				</div>
+				<button className={classes.Admin} onClick={switchAdmin}>
+					Admin
+				</button>
+			</div>
+
+			{admin ? <Admin closePanel={() => closePanel()} /> : null}
+		</div>
+	);
+};
 
 export default Footer;
